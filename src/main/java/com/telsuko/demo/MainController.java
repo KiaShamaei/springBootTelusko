eb5f3c4f2eecd33aabbf36f1dc8cfb1065e9486f
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-18 19:12:45 +0330

    use HttpServlet to get data from client

--- src/main/java/com/telsuko/demo/MainController.java
+++ src/main/java/com/telsuko/demo/MainController.java
@@ -4,11 +4,18 @@ package com.telsuko.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
     @RequestMapping("home")
-    public String home (){
+    public String home (HttpServletRequest req){
+        HttpSession session = req.getSession() ;
+
+        String name = req.getParameter("name");
+        session.setAttribute("name" , name);
         System.out.println("hello this first page !");
-        return "home.jsp";
+        return "home";
     }
 }
--- src/main/webapp/pages/home.jsp
+++ src/main/webapp/pages/home.jsp
@@ -6,6 +6,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body>
-<h1>this is the first page !</h1>
+<h1>this is the first page ! ${name}</h1>
 </body>
 </html>
